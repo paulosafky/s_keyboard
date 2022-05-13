21b57cf3f69e93a069024a85f2a7685355d4912f
SAFKY <62437089+paulosafky@users.noreply.github.com>
2022-05-13 16:44:37 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'SAFKY'
+description 's_keyboard'
+version '1.0.0'
+
+client_script 'client/main.lua'
+server_script 'server.lua'
+ui_page 'html/index.html'
+files{
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
